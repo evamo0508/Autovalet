7b0fee72c9bd4e2bc6dfbfdbb9216ea4b0fcc2bf
evamo0508 <evamo.tw1@gmail.com>
2020-03-20 18:32:37 -0400

    ground plane w/ yellow lines
    rtab simulation, img size revised
    
    updated urdf to have sensor placements for a ford focus
    
    realsense plugin img size 640x480
    
    renamed garage image file
    
    added second garage plane file for testing
    
    added darker ground plane
    
    first rough commit of lane_detection node
    
    rtab finished, merged pcl
    
    utils added
    
    added all ground plane images
    
    Green lanes added
    
    lane GT utils
    
    corrected urdf after testing
    
    delete loam_velodyne dir

---
+++ autovalet_gazebo/src/concatenatePCL.cpp
@@ -0,0 +1,54 @@
+#include <ros/ros.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+
+#include <tf/transform_listener.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <geometry_msgs/TransformStamped.h>
+#include <sensor_msgs/PointCloud2.h>
+
+using namespace message_filters;
+
+tf::TransformListener* pListener;
+
+ros::Publisher cloud_pub;
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+void callback(const PointCloud::ConstPtr& velPtr, const PointCloud::ConstPtr& rsPtr)
+{
+  // transform rs pcl to vel frame
+  PointCloud rs2vel;
+  pcl_ros::transformPointCloud("velodyne", *rsPtr, rs2vel, *pListener);
+
+  // concatenate two pcls 
+  PointCloud pcl_concat;
+  pcl_concat = *velPtr;
+  pcl_concat += rs2vel;
+
+  // republish new topic
+  cloud_pub.publish(pcl_concat);
+
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "concat_pcl");
+  ros::NodeHandle nh;
+  
+  pListener = new tf::TransformListener();
+  
+  message_filters::Subscriber<PointCloud> vel_sub(nh, "/velodyne_points", 1);
+  message_filters::Subscriber<PointCloud> rs_sub(nh, "/realsense/frontCamera/depth/points", 1);
+  typedef sync_policies::ApproximateTime<PointCloud, PointCloud> MySyncPolicy;
+  Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), vel_sub, rs_sub);
+  sync.registerCallback(boost::bind(&callback, _1, _2));
+  cloud_pub = nh.advertise<PointCloud> ("/concat_points", 1);
+  
+  ros::spin();
+}
