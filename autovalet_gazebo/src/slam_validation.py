a2726d8db25ed46f598afd14041635134c0328c5
Kevin O'Brien <kobrien.1002@gmail.com>
2020-04-15 19:39:25 -0400

    updates to slam val script

--- autovalet_gazebo/src/slam_validation.py
+++ autovalet_gazebo/src/slam_validation.py
@@ -39,27 +39,29 @@ class Error_Calculator:
 				        ["TL","BL"],
 				        ["TR","BR"],
 				        ["TR","BR"]]
-		self.corner_points = self.calc_corners(self.box_points,self.corners)
+		self.box_list = [0,13]
+		self.corner_points = self.calc_corners(self.box_points,self.corners,self.box_list)
 
 	# calculate box corner points given box centroids and directions
 	# talk to kevin about this, he knows what it means... :)
-	def calc_corners(self,points,directions):
+	def calc_corners(self,points,directions,box_list):
 	    points_list = []
-	    for i in range(len(directions)):
+	    for i in range(len(box_list)):
+	    	index = box_list[i]
 	        # print(temp_point)
-	        for j in range(len(directions[i])):
-	            temp_point = points[i].copy()
+	        for j in range(len(directions[index])):
+	            temp_point = points[index].copy()
 	            temp_point[2] += 0.5
-	            if directions[i][j] == "TR":
+	            if directions[index][j] == "TR":
 	                temp_point[0] += 0.5
 	                temp_point[1] -= 0.5
-	            if directions[i][j] == "BR":
+	            if directions[index][j] == "BR":
 	                temp_point[0] -= 0.5
 	                temp_point[1] -= 0.5
-	            if directions[i][j] == "TL":
+	            if directions[index][j] == "TL":
 	                temp_point[0] += 0.5
 	                temp_point[1] += 0.5
-	            if directions[i][j] == "BL":
+	            if directions[index][j] == "BL":
 	                temp_point[0] -= 0.5
 	                temp_point[1] += 0.5
 	            # print(temp_point)
@@ -108,21 +110,29 @@ class Error_Calculator:
 
 		map_point = np.array([map_x,map_y,map_z,1])
 		
-
+		if self.i % 2 == 0:
+			print "BOX",self.box_list[self.i/2]
 		map_point = map_point[:3]
-		print("generated",map_point)
+		print "Map-Generated Coordinate:", map_point
 
 		gt_point = np.dot(self.map_frame_transform, self.corner_points[self.i])
 
 		gt_point = gt_point[:3]
-		print("ground truth", gt_point)
+		print "Ground Truth Coordinate:", gt_point
+		diff = map_point - gt_point
+		error = np.linalg.norm(map_point - gt_point)	
+		print "dx:", diff[0], "m"
+		print "dy:", diff[1], "m"
+		print "dz:",diff[2],"m"
+		print "Error:", error, "m"
+		print
 
 		self.i += 1
-		self.dist += np.linalg.norm(map_point - gt_point)
+		self.dist += error
 
 		avg = self.dist / self.i
 
-		print("Average Error:", avg, "m")
+		print "Compounded Average Error:", avg, "m"
 		print("*********")
 		if self.i == self.num_points:
 			print("Error test completed")
