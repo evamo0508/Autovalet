c8f5b5c152815928e336b5f7c9f052e7d6e0e54f
Subramanian Krishnan <subramanian.krishnan@vecnarobotics.com>
2020-09-08 21:10:41 -0400

    Changed the ground plane to be artificially green Added online segmentation map generating script
    Added the goal generation package
    Corrected the namespace for realsense in simulation
    
    Added lane segmentation in cloud
    
    Added the goal generation package
    Corrected the namespace for realsense in simulation
    
    Added lane segmentation in cloud
    
    Added lane segmenter in cloud to project the segmented depth map of the lane back onto the real world
    Added a fake "lane" sensor in the costmap commons params file
    
    /realsense namespace removed
    depth image registered to rgb image
    lane pcl projected onto constmap
    goal transfer launch files created
    
    removed static params yaml and updated global costmap params
    
    removed static map params and updated global cmap params for sim and hw
    
    Changed the ground plane to be artificially green
    Added online segmentation map generating script
    
    Added lane segmentation in cloud
    
    segmap uncommented in launch file

---
+++ lane_detection/scripts/generate_gt_segmap.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+Script publishes a segmentation map from the artificially colored
+ground plane for transferring the lane constraint to costmap
+
+Author  : Subramanian Krishnan (subramak@andrew.cmu.edu)
+Date    : 07 Sep 2020
+
+Changelog:
+    subbu - 9/07 - Initial commit
+"""
+
+import rospy
+import sys
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+import cv2
+
+class ground_truth:
+    '''
+    Wrapper class to process the "artificial" ground plane
+    '''
+    def __init__(self, output_topic, input_topic):
+        self.pub = rospy.Publisher(output_topic, Image, queue_size=1)
+        self.sub = rospy.Subscriber(input_topic, Image, self.callback)
+        self.bridge = CvBridge()
+
+    def callback(self, rosImage):
+        # process image
+        img = self.bridge.imgmsg_to_cv2(rosImage,"bgr8")
+
+        # Convert BGR to HSV
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        # define range of blue color in HSV
+        # Range found from the hsv configuration tool in utils
+        low = np.array([48,156,222])
+        high = np.array([150,255,255])
+
+        # Threshold the HSV image to get only blue colors
+        mask = cv2.inRange(hsv, low, high)
+        kernel = np.ones((7,7),np.uint8)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+        try:
+            self.pub.publish(self.bridge.cv2_to_imgmsg(mask, "mono8"))
+        except CvBridgeError as e:
+            print(e)
+def main():
+  rospy.init_node('lane_ground_truth', anonymous=False)
+
+  gt_topic = "/lane/ground_truth"
+  input_topic = "/realsense/frontCamera/color/image_raw"
+
+  gt = ground_truth(gt_topic, input_topic)
+
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+
+if __name__ == '__main__':
+    main()
