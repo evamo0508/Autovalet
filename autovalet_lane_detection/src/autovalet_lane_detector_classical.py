40fd26a492aa2eb4aecf2c415bbac2b07c93d023
evamo0508 <evamo.tw1@gmail.com>
2020-09-11 13:21:16 -0400

    line detection

--- autovalet_lane_detection/src/autovalet_lane_detector_classical.py
+++ autovalet_lane_detection/src/autovalet_lane_detector_classical.py
@@ -33,49 +33,44 @@ class LaneDetector:
         beta = 30
         img  = cv2.convertScaleAbs(img, alpha=1, beta=beta)
         #===================================================
-        res = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS).astype(np.float)
 
         # hls thresholding for yellow
         lower = np.array([40/2, 0.00*255, 0.55*255], dtype=np.uint8)
         upper = np.array([60/2, 1.00*255, 1.00*255], dtype=np.uint8)
-        #lower = np.array([20, 120, 80], dtype=np.uint8)
-        #upper = np.array([45, 200, 255], dtype=np.uint8)
         mask = cv2.inRange(hls, lower, upper)
-        th = cv2.bitwise_and(res, res, mask=mask).astype(np.uint8)
+        th = cv2.bitwise_and(rgb, rgb, mask=mask).astype(np.uint8)
         th = cv2.cvtColor(th, cv2.COLOR_HLS2RGB)
         th = cv2.cvtColor(th, cv2.COLOR_RGB2GRAY)
-        #_, th = cv2.threshold(th, 20, 255, cv2.THRESH_BINARY)
-        out = np.zeros_like(th)
-        out[(th >= 60) & (th <= 255)] = 255
+        th = cv2.GaussianBlur(th, (5, 5), 0)
+        _, th = cv2.threshold(th, 60, 255, cv2.THRESH_BINARY)
 
-
-        """
-        # mask gray with ROI
-        mask = np.zeros_like(gray)
+        # extract ROI
+        mask = np.zeros_like(th)
         mask[self.ROI_UPPER_Y:, :self.ROI_RIGHT_X] = 1
-        gray_roi = cv2.bitwise_and(gray, gray, mask=mask)
+        roi = cv2.bitwise_and(th, th, mask=mask)
 
         # detect center line
-        edges = cv2.Canny(gray_roi, 150, 200, apertureSize=3)
-        minLineLength = 30
-        maxLineGap = 5
-        lines = cv2.HoughLinesP(edges, cv2.HOUGH_PROBABILISTIC, np.pi/180, 30, minLineLength, maxLineGap)
+        edges = cv2.Canny(roi, 50, 150, apertureSize=3)
+        minLineLength = 50
+        maxLineGap = 20
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 30, np.array([]), minLineLength, maxLineGap)
 
-        # draw line on gray_roi
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(res, (x1, y1), (x2, y2), (255, 0, 0), 3)
-        """
+        # draw line on res
+        res = np.stack((th, th, th), axis=-1)
+        if lines is not None:
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                cv2.line(res, (x1, y1), (x2, y2), (0, 0, 255), 3)
 
-        # draw rectangle on  ROI
+        # draw rectangle on ROI
         h, w, _ = img.shape
         self.ROI_UPPER_LEFT = (0, self.ROI_UPPER_Y)
         self.ROI_LOWER_RIGHT = (self.ROI_RIGHT_X, h)
         color = (255, 0, 0) # blue in BGR
         thickness = 2
-        res = np.stack((out, out, out), axis=-1)
         res = cv2.rectangle(res, self.ROI_UPPER_LEFT, self.ROI_LOWER_RIGHT, color, thickness)
 
         # concatenate data & result
