418b8b8e7eaadd2ede592c3a3c0e72c3590e963d
莫絲羽 <evamo.tw1@gmail.com>
2020-10-24 16:46:33 -0400

    Generate 2 parking waypoints wrt AruCo detection (#12)
    Co-authored-by: Nikita <nikitam@andrew.cmu.edu>

--- autovalet_gazebo/scripts/validate_localization.py
+++ autovalet_gazebo/scripts/validate_localization.py
@@ -56,17 +56,17 @@ class Localization_validation:
         if(self.last_gt is not None and self.gt is not None):
             cur_pose = np.array([self.estimate.position.x, self.estimate.position.y, self.quat_to_euler(self.estimate.orientation)[2]])
             gt_pose = np.array([self.gt.position.x, self.gt.position.y, self.quat_to_euler(self.gt.orientation)[2]])
-            
+
             dist_moved = np.linalg.norm(gt_pose[:-1] - self.last_gt[:-1])
             # print "current_error_heading", current_error_heading*180/3.14
             # print " **************************************************"
             if(dist_moved > self.sample_dist):
                 current_error_position = np.linalg.norm(cur_pose[:-1] - (gt_pose[:-1] - self.offset[:-1]))
                 current_error_heading = np.abs(cur_pose[-1] - (gt_pose[-1] - self.offset[-1]))
-                
-                print "Current Ground truth (in cms, degrees: x,y, theta)", (gt_pose[:-1] - self.offset[:-1])*100, (gt_pose[-1] - self.offset[-1])*180/3.14 
+
+                print "Current Ground truth (in cms, degrees: x,y, theta)", (gt_pose[:-1] - self.offset[:-1])*100, (gt_pose[-1] - self.offset[-1])*180/3.14
                 print "Current odometry (in cms, degree: x,y,theta)", cur_pose[0]*100, cur_pose[1]*100, cur_pose[2]*180/3.14
-                print "Individual Errors (in cms, degree: x, y, theta) ", (cur_pose - (gt_pose - self.offset))[:-1]*100, (cur_pose - (gt_pose - self.offset))[-1]*180/3.14 
+                print "Individual Errors (in cms, degree: x, y, theta) ", (cur_pose - (gt_pose - self.offset))[:-1]*100, (cur_pose - (gt_pose - self.offset))[-1]*180/3.14
                 print "Current Euclidean error (in cms, degrees): ", current_error_position*100, current_error_heading*180/3.14
                 print "==============================================="
                 print "\n\n"
@@ -79,7 +79,7 @@ class Localization_validation:
                 np.save(self.filename+"_pError", self.positionErrorLog)
                 np.save(self.filename+"_aError", self.runningAverage)
                 np.save(self.filename+"_yError", self.yawErrorLog)
-                
+
     def quat_to_euler(self, q):
         # euler indexing as RPY
         quaternion = (q.x, q.y, q.z, q.w)
@@ -92,7 +92,7 @@ def main():
 
     print("Starting Localization Error calculator node")
     rospy.init_node("localization_validation", anonymous=False)
-    
+
     ground_truth_topic = '/gazebo/link_states'
     # estimate_topic = '/odometry/filtered'
     estimate_topic = '/icp_odom'
---
+++ autovalet_gazebo/scripts/validate_parking.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# license removed for brevity
+
+# Author: Eva Mo
+# Adapted from: validate_arucotags.py
+# Date of creation: October 9, 2020 04:20 PM
+# Description: Validation script for Progress Review 9, compare husky final pose with arucotags gt.pose + 4m into the right spot
+
+import os
+import numpy as np
+
+import rospy
+from gazebo_msgs.msg import LinkStates
+from tf.transformations import euler_from_quaternion
+
+class parking_pose_validation:
+    def __init__(self, gt_topic):
+        # gt: ground truth published by gazebo link state
+        self.gt = rospy.wait_for_message(gt_topic, LinkStates)
+        self.aruco_tag_gt = None
+        self.base_link_gt = None
+
+        self.target_pose = self.get_target_pose()
+        self.reached_pose = self.get_reached_pose()
+        self.calculate_error()
+
+    def get_target_pose(self):
+        self.aruco_tag_gt    = self.gt.pose[-7] # name 'aruco_visual_marker_7::marker'
+        # x += 4 compensating for the translation between parking spot & tag
+        aruco_tag_gt_2d = np.array([self.aruco_tag_gt.position.x , self.aruco_tag_gt.position.y - 4, self.quat_to_euler(self.aruco_tag_gt.orientation)[2]])
+
+        return aruco_tag_gt_2d
+
+    def get_reached_pose(self):
+        self.base_link_gt    = self.gt.pose[-5] # base_link w.r.t the gazebo world origin
+        base_link_gt_2d = np.array([self.base_link_gt.position.x, self.base_link_gt.position.y, self.quat_to_euler(self.base_link_gt.orientation)[2]])
+
+        return base_link_gt_2d
+
+    def calculate_error(self):
+        # Error in x and y in cms
+        trans_err = (self.target_pose[:-1] - self.reached_pose[:-1])*100
+        # Error in orientation (yaw) in degrees
+        # make sure the orientation is the same in hardware so that the "270" works as well
+        yaw_err   = np.abs((self.target_pose[2]*180/3.14 - 270) - self.reached_pose[2]*180/3.14) #comparing the aruco tag (rotated by 270deg) with husky's yaw
+        print "Target pose (x,y,theta): ", self.target_pose[:-1]*100, self.target_pose[2]*180/3.14 - 270
+        print "Reached pose (x,y,theta): ", self.reached_pose[:-1]*100, self.reached_pose[2]*180/3.14
+        print "Translation error (cms): ", trans_err
+        print "Orientation error (deg): ", yaw_err
+
+    def quat_to_euler(self, q):
+        # euler indexing as RPY
+        quaternion = (q.x, q.y, q.z, q.w)
+        euler = euler_from_quaternion(quaternion)
+        return euler
+
+
+def main():
+    np.set_printoptions(suppress=True, precision=4)
+
+    print("Starting Parking Pose Error calculator node")
+    rospy.init_node("parking_pose_validation", anonymous=False)
+
+    ground_truth_topic = '/gazebo/link_states'
+
+    errorHandler = parking_pose_validation(ground_truth_topic)
+
+    while not rospy.is_shutdown():
+        pass
+
+if __name__ == '__main__':
+    main()
