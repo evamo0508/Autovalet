931e2c11083c91d3112b619d9c57166f1c2dfe05
Nikita <nikitam@andrew.cmu.edu>
2020-11-07 15:31:44 -0500

    Left-right parking both working

--- autovalet_husky/src/autovalet_husky/find_parking_spot.py
+++ autovalet_husky/src/autovalet_husky/find_parking_spot.py
@@ -42,7 +42,6 @@ class parking_spot:
         self.first_goal_close = False
         self.debug = debug
         self.line = None
-        self.linecenter = None
 
         # helper const & bool
         self.costmap_height              = rospy.get_param('/move_base/global_costmap/height')
@@ -72,26 +71,21 @@ class parking_spot:
 
     def pub_two_goals(self):
 
-        self.linecenter = self.line # to stop recalc of centerline midpt
-        if self.linecenter.y > self.tag_tf.transform.translation.y:
+        if self.line.y > self.tag_tf.transform.translation.y: 
             rhs = True
             pos1 = [0, 3, 1]
-            # rot1 = [0 , np.pi/4, -np.pi/4] #need to switch
+            rot1 = [0 , -np.pi/4, -np.pi/4] 
 
         else: #left park
             rhs = False
             pos1 = [0, 1.5, -1]
-            # rot1 = [0 , np.pi/4, -np.pi/4]
-            print("line center y value : ", self.linecenter.y)
-            print("tag y : ", self.tag_tf.transform.translation.y)
-        # current params for right turns only
-        rot1 = [0 , np.pi/4, -np.pi/4] 
+            rot1 = [0 , np.pi/4, -np.pi/4]
         self.goal1 = self.generate_parking_goal(self.tag_tf, pos1, rot1)
         
         # keep moving w/ goal gen until goal1 is in costmap
         while self.dist_to_goal(self.goal1) > 0.48 * self.costmap_height:
             # rospy.sleep(0.1)
-            a = 1 #continue
+            continue #a=1
         self.first_goal_in_costmap = True
 
         # keep publishing 1st goal until the robot is close enough
@@ -102,20 +96,14 @@ class parking_spot:
                 print("first goal published")
 
         # pub 2nd goal once
-        if rhs: #self.linecenter.y > self.goal1.pose.position.y:
+        if rhs:
             pos2 = [0, 0, 4]
             rot2 = [0, -np.pi/2, -np.pi/2]
-            if self.debug:
-                print"**********right spot*********"
-                print ("tfs are: ", self.linecenter.y, self.goal1.pose.position.y)
+
         else:
             pos2 = [0, 0, -4]
             rot2 = [0, np.pi/2, np.pi/2]
-            if self.debug:
-                print"***********left spot********"
-                print ("tfs are: ", self.linecenter.y, self.goal1.pose.position.y)
-        # pos2 = [0, 0, 4]
-        # rot2 = [0, -np.pi/2, -np.pi/2]
+
         self.goal2 = self.generate_parking_goal(self.tag_tf, pos2, rot2)
         self.pub.publish(self.goal2)
         if self.debug:
