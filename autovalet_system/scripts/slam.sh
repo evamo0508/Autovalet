b41cb6ba3ead1d11e5d48ad46e3df864950d33f1
Kevin O'Brien <kobrien.1002@gmail.com>
2020-11-09 01:41:46 -0500

    Finalizing parking logic on all 4 worlds, added argument into state_machine.launch for parking and turning

--- autovalet_system/scripts/slam.sh
+++ autovalet_system/scripts/slam.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 sim=$1
 turn=$2
+park=$3
+
 if [ "$sim" = "true" ]
 then
     if [ "$turn" = "right" ]
     then
-        roslaunch autovalet_gazebo setup_slam_test.launch world_file:=state_machine.world x:=0 y:=6 1>> $HOME/slam_log.txt 2>&1 #0,6 # use (-15,-9) for rightside debugging
+        if [ "$park" = "right" ]
+        then
+            # right turns, right park
+            roslaunch autovalet_gazebo setup_slam_test.launch world_file:=state_machine_rturns_rpark.world x:=0 y:=6 1>> $HOME/slam_log.txt 2>&1
+        else
+            # right turns, left park
+            roslaunch autovalet_gazebo setup_slam_test.launch world_file:=state_machine_rturns_lpark.world x:=0 y:=6 1>> $HOME/slam_log.txt 2>&1
+        fi
     else
-        roslaunch autovalet_gazebo setup_slam_test.launch world_file:=state_machine_lturns_lpark.world x:=-6 y:=-9 1>> $HOME/slam_log.txt 2>&1
-    fi
+        if [ "$park" = "right" ]
+        then
+            # left turns, right park
+            roslaunch autovalet_gazebo setup_slam_test.launch world_file:=state_machine_lturns_rpark.world x:=-6 y:=-9 1>> $HOME/slam_log.txt 2>&1
+        else
+            # left turns, left park
+            roslaunch autovalet_gazebo setup_slam_test.launch world_file:=state_machine_lturns_lpark.world x:=-6 y:=-9 1>> $HOME/slam_log.txt 2>&1
+        fi  
+    fi 
 else
     roslaunch autovalet_husky integrated_sensor.launch 1>> $HOME/sensor_log.txt 2>&1
     roslaunch autovalet_husky valet_rtab.launch 1>> $HOME/slam_log.txt 2>&1
